a081f6e1f05de548747fa66af4c9b1c4c2098949
Wouter van Bommel <wouter@vanbommelonline.nl>
2012-04-13 15:29:07 +0200

    rename README to README.md

--- dpkg.py
+++ dpkg.py
@@ -6,6 +6,7 @@ This file will implement the dpkg class
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 import subprocess
+import checkers
 
 class Dpkg:
     """This class will implement the internals of the dpkg handling"""
@@ -13,6 +14,9 @@ class Dpkg:
         """Set some local variables"""
         self._dpkg_exe = dpkg_exe
         self._dpkg_dir = dpkg_dir
+        self._config_checker_list = list()
+        for checker in checkers.checkerslist:
+            self._config_checker_list.append(checker)
 
     def make_dpkg_listing(self):
         """Make a listing of installed packages"""
@@ -25,6 +29,17 @@ class Dpkg:
             if status == 'install':
                 _new_list.append(pkg)
         return _new_list
+
+    def find_altered_config(self, pkg):
+        """Return a list of config files to be included based on the package
+        name.
+        """
+        files = list()
+        for checker in self._config_checker_list:
+            retval = checker.do_check(pkg)
+            if retval:
+                files.append(retval)
+        return files
 
 if __name__ == '__main__':
     # No unit test implemented yet
